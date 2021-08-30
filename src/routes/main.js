7b32941039b7b922baab22a0d95ecb3d5d321abe
LucioMart <luchoweb15@gmail.com>
2021-08-30 09:36:42 -0300

    Definicion de las rutas.

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const mainController = require('../controllers/mainController')
+const {index, admin, login, register, products, detail} = require('../controllers/mainController')
 
-router.get('/', mainController.index)
+router.get('/', index)
+router.get('/admin', admin)
+router.get('/login', login)
+router.get('/register', register)
+router.get('/detail', detail)
+router.get('/products', products)
 
 module.exports = router
